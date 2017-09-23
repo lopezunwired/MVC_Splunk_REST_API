6926044dc97d8375d38cb681908138c68aa9d8b2
Paul Lopez <paul_lopez1@optum.com>
2017-09-23 14:00:17 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # MVC_Splunk_REST_API
 Splunk REST API MVC App
+Visual Studio 2017 Simple Splunk REST API with MVC App
+
+This project file is a simple MVC application to show you how easy it is to connect to a Splunk instance with the REST API.
+Be sure to set "allowremotelogin=always" in your Splunk "local" configuration file.
+
+For more information:
+http://docs.splunk.com/Documentation/Splunk/6.6.3/RESTUM/RESTusing
+
+
+Edit: appsettings.json
+set you localhost where your Splunk Server is running on port 8089.
+Set your username and password to that splunk instance
+
+All of the relevant code is in the HomeController.cs file
+
+The program retrieves example log data from the Splunk server. 
---
+++ SplunkCore/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+
+namespace SplunkCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IOptions<SplunkConfig> config;
+        private readonly string SPLUNKRESTAPIURL = string.Empty;
+        private readonly string UserName = string.Empty;
+        private readonly string Password = string.Empty;
+        private readonly object JsonRequestBehavior;
+
+        public HomeController(IOptions<SplunkConfig> config)
+        {
+            this.config = config;
+           this.SPLUNKRESTAPIURL= config.Value.SplunkRestApiUrl;
+            this.UserName = config.Value.UserName;
+            this.Password = config.Value.Password;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, string>> GetAuthenticate()
+        {
+            string Key = string.Empty;
+            using (var handler = new HttpClientHandler())
+            {
+                handler.ServerCertificateCustomValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+                using (var _client = new HttpClient(handler))
+                {
+                    try
+                    {
+                        _client.BaseAddress = new Uri(SPLUNKRESTAPIURL);
+                        var request = new HttpRequestMessage(HttpMethod.Post, "/services/auth/login");
+                        request.Content = new FormUrlEncodedContent(new[]{
+                        new KeyValuePair<string, string>("username", UserName),
+                        new KeyValuePair<string, string>("password", Password)});
+                        var _result1 = Task.Run(async () => await _client.SendAsync(request)).Result;
+                        string __result = _result1.Content.ReadAsStringAsync().Result;
+                        XElement incomingXml = XElement.Parse(__result);
+                        string key = (string)incomingXml.Element("sessionKey");
+                        Key = key;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+            }
+            var _dic = new Dictionary<string, string>();
+            _dic.Add("SessionKey", Key);
+            return _dic;
+        }
+        [HttpGet]
+        public JsonResult GetData(string sessionkey)
+        {
+            string __result = string.Empty;
+            using (var handler = new HttpClientHandler())
+            {
+                handler.ServerCertificateCustomValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+                using (var _client = new HttpClient(handler))
+                {
+                    try
+                    {
+                        _client.BaseAddress = new Uri(SPLUNKRESTAPIURL);
+                        var request = new HttpRequestMessage(HttpMethod.Get, "/servicesNS/admin/search/data/lookup-table-files?output_mode=json");
+                        request.Headers.Add("Authorization",string.Format("Splunk {0}", sessionkey));
+                        var _result1 = Task.Run(async () => await _client.SendAsync(request)).Result;
+                        __result = _result1.Content.ReadAsStringAsync().Result;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+            }
+            return Json(__result);
+        }
+    }
+}
